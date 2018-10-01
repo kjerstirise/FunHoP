7364488aa74f4060dc523187b671c00d23ee95d5
Kjersti Rise <kjersti.rise@ntnu.no>
2018-10-01 10:45:20 +0200

    Cleaning up and adding comments

--- fix_coordinates.py
+++ fix_coordinates.py
@@ -5,12 +5,18 @@ import xml.etree.ElementTree as ET
 import copy
 import glob
 import pandas as pd
+"""
+This code tries to improve the outputfiles, making it easier for the user to view and work with the files with extended 
+nodes in Cytoscape. Fix_y changes the coordinates of all nodes below a node that is extended, making the number of overlapping 
+nodes smaller. 
 
+"""
 
 def fix_y(root):
 
-	result = []
+	nodes = []
 
+	#Extract the genes and compounds 
 	for child in root:
 		for underchild in child:
 			if (child.attrib["type"] == "gene") or (child.attrib["type"] == "compound"):
@@ -20,63 +26,39 @@ def fix_y(root):
 				else:
 					split_namestring = namestring.split(" ")
 					length = len(split_namestring)
-				result.append((int(child.attrib["id"]), int(length), int(underchild.attrib["y"])))
+				nodes.append((int(child.attrib["id"]), int(length), int(underchild.attrib["y"])))
 
-	#print(result)
-	results = pd.DataFrame(result)
-	results.columns = ["ID", "length", "y"]
-	#results.reset_index(drop = True, inplace = True)
-	results.sort_values(by = ['y'], ascending = True, inplace = True)
-	results = results.apply(pd.to_numeric, errors = 'ignore')
-	#print(results)
-
-	listres = results.values.tolist()
-	print(type(listres))
-	#[' '.join(x) for x in listres]
-
-	"""	
-	y = 0
-	for i, column in results.iterrows():
-		if column['y'] > y:
-			y = column['y']
-			y_string = str(y)
-		if column['length'] > 1:
-			value = column['length'] * 17
-			print(value)
-		for j, column in results.iloc[y_string:].iterrows():
-			#column['y'] = column['y'] + value
-			print("here")
+	nodes_df = pd.DataFrame(genes_and_compounds)
+	nodes_df.columns = ["ID", "length", "y"]
+	nodes_df.sort_values(by = ['y'], ascending = True, inplace = True)
+	nodes_df = nodes_df.apply(pd.to_numeric, errors = 'ignore')
 
 
-	#print(results)
+	nodes_list = nodes_df.values.tolist()
 	
-	result_sorted = result.sort(key = lambda x: x[2])
-	#result_lines = result_sorted.split()
-
-	print(result_sorted)
-	"""
+	
 
 	y = 0
 	counter = 0
-	for row in listres:
+	for row in nodes_list:
 		print(row)
 		counter = counter + 1
 		if row[2] > y:
 			y = row[2]
 
-		# Hvis vi har flere enn 1 gen (flyttetid)
+		# Find the number of genes
 		gene_count = row[1]
 		if gene_count > 1:
 			value = gene_count * 17
 			print(type(value))
 
-			for i in range(counter, len(listres)):	
-				print(listres[i])
-				listres[i][2] = listres[i][2] + value
+			for i in range(counter, len(nodes_list)):	
+				print(nodes_list[i])
+				nodes_list[i][2] = nodes_list[i][2] + value
 
-
+	# Find and replace y-coordinates
 	new_y = {}
-	for  line in listres:
+	for line in nodes_list:
 		new_y[line[0]] = line[2]
 
 	print(new_y) 
@@ -96,11 +78,11 @@ def fix_y(root):
 
 
 def main():
-	tree = ET.parse('/Users/profile/Documents/GitHub/cell-lines/changed_name/changed_name_hsa00340.xml')
+	tree = ET.parse('/Users/profile/Documents/GitHub/cell-lines/changed_name/testmappe/changed_name_hsa00564.xml')
 	#tree = ET.parse('/Users/Kjersti/phd/Kode/boxtester.xml')
 	root = tree.getroot()
 	fix_y(root)
-	tree.write("outfile_testfile.xml")
+	tree.write("outfile_testfile_564.xml")
 
 
 
