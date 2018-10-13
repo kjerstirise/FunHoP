126da15e91817a2f3eac27f7cb12a9d2381a6fde
Kjersti Rise <kjersti.rise@ntnu.no>
2018-10-13 20:05:54 +0200

    Adding highest/lowest values printed to screen
    This makes it easier to create pictures afterwards, as you know which ranges to use in Cytoscape

--- create_pvalues_from_diffexp.py
+++ create_pvalues_from_diffexp.py
@@ -15,17 +15,10 @@ def extract_pvalue():
 
 	diffexp_results = pd.read_table('/Users/profile/Documents/GitHub/cell-lines/diffexp_updated_cancertissueVsDU145_FPKM_oct18.txt',
 		sep = "\t", header = 0, names = None)
-	#diffexp_results = pd.read_table('/Users/profile/phd/sammenligninger/sign_table_canctr4_Kegg_TCGA_feb2017.txt',
-	#	sep = "\t", header = None, names = None)
-	
-	print(diffexp_results.dtypes)
-	print("original")
-	print(diffexp_results.head())
-	print(diffexp_results.size)
+
 	significant_pvalues = diffexp_results.loc[diffexp_results.iloc[:,5] <= 0.05]
 
-	print("Etter redusering på p-verdi")
-	print(significant_pvalues.head())
+	
 	reg = []
 	for row in significant_pvalues['logFC']:
 		if row < 0:
@@ -33,27 +26,24 @@ def extract_pvalue():
 		else:
 			reg.append(int('1'))
 	
+	# This one creates a warning! But it still works..
 	significant_pvalues['regulation'] = reg
 	creating_transformed_pvalues = np.log2(significant_pvalues.iloc[:,5]) * significant_pvalues.iloc[:, 8] * -10
 
+	# Another warning from this one.. 
 	significant_pvalues['trans_pvalues'] = creating_transformed_pvalues
-	print("etter å ha transformert")
-	print(significant_pvalues)
+	
 	significant_pvalues = significant_pvalues.drop(significant_pvalues.columns[[1, 2, 3, 4, 5, 6, 7,8]], axis = 1)
-	print("etter å ha fjernet")
-	print(significant_pvalues.size)
-	print(significant_pvalues.head())
+	
 	final_pvalues = significant_pvalues[significant_pvalues.trans_pvalues.notnull()]
-	print("etter å ha fjernet negative")
-	print(final_pvalues.size)
-	#print(final_pvalues)
+	
+	print("The highest found value is: ")
+	print(final_pvalues.trans_pvalues.astype(float).max())
+	print("The lowest found value is: ")
+	print(final_pvalues.trans_pvalues.astype(float).min())
+	
 	#final_pvalues.to_csv('pvalues_diffexp_FPKM_cancerTissueVsDU145.txt', header = False, index = None, sep = '\t', mode = 'w') 
-	"""
-		pvalues = diffexp_results.loc[diffexp_results.iloc[:,5]] 
-	pvalues.replace('\n',' ', regex = True)
-	print(pvalues.head())
-	significant_pvalues = float(pvalues) <= 0.05
-	"""
+	
 
 
 def main():
