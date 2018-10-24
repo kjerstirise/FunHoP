c5c4999f55f37a97f337f0a1abe5fd4e3b852314
Kjersti Rise <kjersti.rise@ntnu.no>
2018-10-24 20:46:06 +0200

    adding master file

---
+++ funhop.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import change_namestring
+
+
+def main():
+	change_namestring.change_namestring(pathway_path = '/Users/Profile/Documents/GitHub/cell-lines/pathways', hsalist_path)
+
+if __name__ == '__main__':
+    main()
