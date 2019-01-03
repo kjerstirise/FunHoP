556e4f7e3d7f117413284bd41a509b0c5782aee1
Kjersti Rise <kjersti.rise@ntnu.no>
2019-01-03 17:22:36 +0100

    Update remove_loose_metabolites.py

--- remove_loose_metabolites.py
+++ remove_loose_metabolites.py
@@ -25,8 +25,7 @@ def find_unconnected_metabolites(root):
 			all_compounds.append(child.attrib["id"])
 
 	x = list(set(all_compounds))
-	#print(len(x))
-	#print(x)
+
 
 	#Second, identify all compounds found among the relations. 
 	#These will be the compounds that are connected to the pathway
@@ -41,10 +40,7 @@ def find_unconnected_metabolites(root):
 
 
 	y = list(set(all_connected_metabolites))
-	#print(len(y))
-	#print(y)
-	
-	#return(all_connected_metabolites)
+
 	
 	#Third, compare the two lists, to find the compounds that are found in both. 
 	#This step could have been skipped, as the all_connected_metabolites would have been enough, 
@@ -55,9 +51,6 @@ def find_unconnected_metabolites(root):
 		if compound in all_connected_metabolites:
 			connected.append(compound)
 
-	
-	#print(len(connected))
-	#print(connected)
 	return(connected)
 
 
@@ -76,23 +69,17 @@ def remove_unconnected_metabolites(root, connected_metabolites):
 
 
 def remove_loose_metabolites(pathway_path, outfile_path):
-	#tree = ET.parse('/Users/profile/Documents/GitHub/cell-lines/changed_name/testmappe/changed_name_hsa00564.xml')
-	#g = glob.glob('/Users/profile/Documents/GitHub/cell-lines/changed_name/*.xml')
 	g = glob.glob(os.path.join(pathway_path,'*.xml'))
 	
 	for file in g:
-		print(file)
 		filename = file.split("/")
 		out_file_name = outfile_path + "removed_lose_" + filename[8]
-		
 		tree = ET.parse(file)
 		root = tree.getroot()
 		connected_metabolites = find_unconnected_metabolites(root)
 		remove_unconnected_metabolites(root, connected_metabolites)
 		tree.write(out_file_name)
 		
-
-
 
 if __name__ == '__main__':
 	remove_loose_metabolites(pathway_path, outfile_path)
