a1e9c33dc1c7377d514008fe6c48a1c9f26ba7ec
Kjersti Rise <kjersti.rise@ntnu.no>
2019-01-14 16:39:59 +0100

    Added comments for running

--- funhop.py
+++ funhop.py
@@ -17,6 +17,7 @@ import remove_loose_metabolites
 
 def main():
 
+
 	start_folder = '/Users/profile/Documents/GitHub/cell-lines/enkelt/'
 	hsa_file = 'hsalist_july18.txt'
 
@@ -29,10 +30,12 @@ def main():
 										hsalist_path = os.path.join(start_folder,hsa_file), 
 										outfile_path = changed_name)
 
+	print("Namestrings have been changed to include all genes")
 
 	create_genelist.create_genelist(pathway_path = changed_name, 
 									outfile_path = start_folder)
 
+	print("The list of all nodes has been extracted from the pathways")
 
 	changed_removed = os.path.join(start_folder, 'changed_removed/')
 	if not os.path.exists(changed_removed):
@@ -41,6 +44,7 @@ def main():
 	remove_loose_metabolites.remove_loose_metabolites(pathway_path = changed_name, 
 														outfile_path = changed_removed)
 
+	print("Loose metabolites have been removed")
 
 	changed_removed_fixed = os.path.join(start_folder, 'changed_removed_fixed/')
 	if not os.path.exists(changed_removed_fixed):
@@ -49,6 +53,7 @@ def main():
 	fix_coordinates.fix_coordinates(pathway_path = changed_removed, 
 									outfile_path = changed_removed_fixed)
 
+	print("Coordinates have been stretched to make more room for the expanded files")
 
 	changed_removed_fixed_extended = os.path.join(start_folder, 'changed_removed_fixed_extended/')
 
@@ -58,7 +63,7 @@ def main():
 	extend_nodes.extend_nodes(pathway_path = changed_removed_fixed, 
 								outfile_path = changed_removed_fixed_extended)
 
-
+	print("All nodes with more than one gene have been expanded to show all genes")
 
 	#create_connection.find_duplicates(pathway_path = changed_name)
 
@@ -70,6 +75,8 @@ def main():
 											boxinfo_path = os.path.join(start_folder, 'testtable_tcga_boxinfo2.txt'), 
 											expression_table_path = os.path.join(start_folder, 'test_expression.txt'))
 
+	print("The counts for each node have been calculated")
+
 	collapsed_nodes = os.path.join(start_folder, 'collapsed_nodes2/')
 
 	if not os.path.exists(collapsed_nodes):
@@ -79,9 +86,13 @@ def main():
 									outfile_path = collapsed_nodes, 
 									duplicates = duplicates)
 
+	print("Nodes with multiple genes have been collapsed, in order to show simpler pathways")
+
 
 	calculate_single_counts.calculate_single_counts(counts = os.path.join(start_folder, 'testtable_tcga_boxinfo.txt'),
 													 outfilepath = os.path.join(start_folder, 'single_counts.txt'))
+
+	print("The count for each single gene has been calculated based on the weights from the multiple gene nodes")
 """
 	try:
 		shutil.rmtree(changed_name)
